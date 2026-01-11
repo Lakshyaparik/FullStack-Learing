68ba49ff05a4a77feca0406c00f4656090fd0dd2
lakshya <lakshyaparik99@gmail.com>
2026-01-11 11:30:23 +0530

    phase1-products card are showed in product page

---
+++ JsRevision/jsProject/E-commerce_web/data/productdata.js
@@ -0,0 +1,50 @@
+const products = [
+  {
+    id: 1,
+    name: "Black Strip shirt",
+    price: 2999,
+    image: "http://127.0.0.1:5500/JsRevision/jsProject/E-commerce_web/images/image1.png",
+    categories: ['shirt','hoodie','mens'],
+    description: "Wireless headphones with noise cancellation and deep bass"
+  },
+  {
+    id: 2,
+    name: "Smart Watch",
+    price: 4999,
+    image: "http://127.0.0.1:5500/JsRevision/jsProject/E-commerce_web/images/image2.png",
+    categories: ["electronics", "wearable", "fitness"],
+    description: "Smart watch with fitness tracking and heart rate monitor"
+  },
+  {
+    id: 3,
+    name: "Running Shoes",
+    price: 2499,
+    image: "http://127.0.0.1:5500/JsRevision/jsProject/E-commerce_web/images/image3.png",
+    categories: ["fashion", "sports"],
+    description: "Comfortable running shoes for daily workout and jogging"
+  },
+  {
+    id: 4,
+    name: "Backpack",
+    price: 1799,
+    image: "http://127.0.0.1:5500/JsRevision/jsProject/E-commerce_web/images/image4.png",
+    categories: ["fashion", "travel"],
+    description: "Durable backpack suitable for travel, college, and office"
+  },
+  {
+    id: 5,
+    name: "Bluetooth Speaker",
+    price: 1999,
+    image: "http://127.0.0.1:5500/JsRevision/jsProject/E-commerce_web/images/image5.png",
+    categories: ["electronics", "audio"],
+    description: "Portable Bluetooth speaker with powerful sound output"
+  },
+  {
+    id: 6,
+    name: "Men's Casual T-Shirt",
+    price: 899,
+    image: "http://127.0.0.1:5500/JsRevision/jsProject/E-commerce_web/images/image6.png",
+    categories: ["fashion", "clothing"],
+    description: "Comfortable cotton casual t-shirt for daily wear"
+  }
+];
