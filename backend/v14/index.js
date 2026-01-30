f699a2dbdaf23e6bcccbe04b8c1fcdf3445f898b
lakshya <lakshyaparik99@gmail.com>
2026-01-30 18:52:40 +0530

    a fully curd operation for for register users and cards for show case with edit and delete functionlity

--- backend/v14/index.js
+++ backend/v14/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const userModel = require("./models/usermodel");
 const app = express();
 const path = require("path");
-const { log } = require("console");
 const { name } = require("ejs");
+const { log } = require("console");
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,22 +15,42 @@ app.get("/", (req, res) => {
   res.render('index', { title: 'Home Page' });
 });
 
-app.post('/',async (req, res) => {
-    await userModel.create({
-    name: req.body.name,
-    email: req.body.email,
-    desc: req.body.desc,
-    image: req.body.profileImg
-  })  
-  console.log('while creating : ',req.body);
-  res.redirect('/');
-  
-})
 app.get("/users", async (req, res) => {
   const users = await userModel.find(); 
   res.render('users', { title: 'Users Page',users});
-  log(users);
 });
+
+app.get("/edit/:_id", async(req, res) => {
+  const user = await userModel.findOne({_id: req.params._id})
+  res.render('edit', { title: 'Edit Page', user });
+});
+
+app.get("/delete/:_id", async(req, res) => {
+ await userModel.findOneAndDelete({_id: req.params._id})
+ res.redirect('/users');
+});
+
+
+app.post('/create',async (req, res) => {
+   let {name,email,desc,image} = req.body;
+    await userModel.create({
+    name,   //passing values in the model to create a new user dcocument
+    email,
+    desc,
+    image
+  })  
+  res.redirect('/');
+})
+
+app.post("/update/:_id", async (req, res) => {
+  let {name,email,desc,image}= req.body;
+  await userModel.findByIdAndUpdate(
+    {_id: req.params._id},
+    {name,email,desc,image},
+  )
+  res.redirect('/users');
+});
+
 app.listen(3000, (req, res) => {
   console.log("Server is running on port 3000");
 });
