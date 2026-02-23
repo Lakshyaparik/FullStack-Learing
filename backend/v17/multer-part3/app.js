01801ddf1114c540b76579569bf530311d48ad04
lakshya <lakshyaparik99@gmail.com>
2026-02-23 20:27:49 +0530

    multer part 3--user can upload profile picture through multer

---
+++ backend/v17/multer-part3/app.js
@@ -0,0 +1,232 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const userModel = require('./models/user')
+const postModel = require('./models/post')
+const { log } = require('console')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+
+const upload = require('./config/multerConfig')
+const user = require('./models/user')
+
+
+
+app.set('view engine','ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.use(cookieParser())
+
+let secretKey = 'lakshya'
+
+//protected fun
+
+function isLoggedIN(req,res,next){
+    if(req.cookies=="")                 //check if token is empty or not
+    res.status(401).json({
+  message: "login first",
+  success: false
+  })
+  //always remember req.cookies.token is a way to get token
+  jwt.verify(req.cookies.token,secretKey,(err,decoded)=>{   //decode the token to get email of user for 
+    if(err)                                                 //for further operation
+      return res.status(401).json({
+        message: "unauthorized user",
+        success: false
+      });
+    req.user = decoded;//put decoded information in object user then put user in req for further operation
+
+    next();  //next calling when user is verified
+    
+  })
+}
+
+//get requests
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/home', (req, res) => {
+  res.render('home')
+})
+
+app.get('/logout', (req, res) => {
+  res.cookie('token',"")
+  res.redirect('/')
+})
+//user profile showing
+app.get('/profile',isLoggedIN,async(req,res)=>{
+  
+  const user = await userModel.findOne({email: req.user.email})  //find user in db via decoded info
+  if(!user)
+    return res.status(404).json({
+  message: "user not found in db",
+  success: false
+});
+
+res.render('profile',{user})  //if user is existed show he/her profile 
+})
+//user posts showing code
+app.get('/my-posts',isLoggedIN, async(req, res) => {
+  let user = await userModel.findOne({email: req.user.email}).populate('posts')
+  if(!user){
+    return res.status(404).json({
+      message: "user not found",
+      success: false
+    })
+  }
+  res.render('my-posts',{user})
+
+})
+//user edit post route
+app.get('/edit-post/:id',async (req, res) => {
+  let post = await postModel.find({_id: req.params.id})
+  if(!post){
+    return res.status(404).json({
+      message: "post not found",
+      success: false
+    })
+  }
+  res.render('edit-post',{post})
+})
+
+//upload profile picture route
+app.get('/upload-profilePic',isLoggedIN,(req,res)=>{
+  res.render('upload-profilePic')
+})
+//get profile picture route
+app.post('/uploadPic',isLoggedIN,upload.single('image'),async(req,res)=>{
+  const user =await userModel.findOneAndUpdate({email: req.user.email},{new:true})
+  user.profilePic = req.file.filename
+  await user.save()
+  res.redirect('/profile')
+})
+//delete post route
+app.get('/delete-post/:id',async (req,res)=>{
+  await postModel.findByIdAndDelete(req.params.id)
+  res.redirect('/my-posts')
+})
+
+//like post route
+app.get('/like-post/:id',isLoggedIN,async(req,res)=>{
+
+  let user = await userModel.findOne({email: req.user.email})
+  let post = await postModel.findOne({_id: req.params.id})
+  if(post.like.indexOf(user._id) === -1){
+    // -1 is get when user has not liked the post because indexOf return -1 when element is not found in array
+    post.like.push(user._id)
+  }
+  else{
+    post.like.splice(post.like.indexOf(user._id),1)//splice is used to remove element from array by index
+  }
+  await post.save()
+  res.redirect('/my-posts')
+})
+
+//update post route
+app.post('/update-post/:id',async(req,res)=>{
+  let {title,description}= req.body
+  let updatedPost = await postModel.findByIdAndUpdate(req.params.id,{
+    title,
+    description
+  },{new:true})
+
+  res.redirect('/my-posts')
+})
+//create post
+app.post('/create-post',isLoggedIN,async(req,res)=>{
+  let {title,description}= req.body
+  let user = await userModel.findOne({email: req.user.email})
+  if(!user)
+    return res.status(404).json({
+      message: "user not found in db",
+      success: false
+    })
+
+  let post = await postModel.create({
+    title,
+    description,
+    user: user._id
+    
+  })
+
+  user.posts.push(post._id)
+  await user.save()
+  
+  res.redirect('/my-posts')
+})
+// post requests
+
+app.post('/register',async(req,res)=>{
+  let {name,email,password}= req.body
+
+  let user= await userModel.findOne({email})
+
+  if(user) return res.status(409).json({
+    success : false,
+    message : "user already exists",
+    message: "user already exists"
+  })
+  
+  bcrypt.hash(password,10,async(err,hash)=>{
+    if(err) return res.status(500).json({
+      success : false,
+      message : "internal server error",
+  })
+
+  let createdUser = await userModel.create({
+      name,
+      email,
+      password:hash
+  })
+
+  //jwt token created for remembering
+  let token = jwt.sign({
+    email:createdUser.email,
+    name: createdUser.name,
+    admin: true
+  },secretKey)
+
+  res.cookie('token',token)
+  res.redirect('/home')
+  })
+
+})
+
+app.post('/login',async(req,res)=>{
+  let {email,password}= req.body
+  let user = await userModel.findOne({email})
+  if(!user) return res.status(404).json({
+    success : false,
+    message : "user not found",
+    message: "user not found"
+  })
+
+  bcrypt.compare(password,user.password,(err,result)=>{
+    if(result){
+      let token = jwt.sign({
+        email:user.email,
+        name: user.name,
+        admin: true
+      },secretKey)
+      res.cookie('token',token)
+      res.redirect('/home')
+    }
+  })
+
+})
+
+
+app.listen(3000);
---
+++ backend/v17/multer-part3/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/mini-Project')
+
+const userSchema = mongoose.Schema({
+  profilePic:{
+    type : String,
+    default : "default.png"
+  },
+  name : {
+    type : String,
+    required : true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  posts:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'post'
+    }
+  ]
+})
+
+module.exports = mongoose.model('user',userSchema)
